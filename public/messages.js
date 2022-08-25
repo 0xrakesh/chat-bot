88595f40fa61d4a16d97b84ce4753136bcd221ac
Rakesh Kumar <70006331+0xrakesh@users.noreply.github.com>
2022-08-25 23:55:32 +0530

    Add files via upload

---
+++ public/messages.js
@@ -0,0 +1,11 @@
+var parent = document.getElementsByClassName("messages");
+    var client = document.getElementsByClassName("bot");
+
+parent[0].innerHTML = 'hi';
+client[0].innerHTML = 'Hi';
+
+parent[1].innerHTML = 'how are you?';
+client[1].innerHTML = 'I am fine.What about you?';
+
+parent[2].innerHTML = 'hi';
+client[2].innerHTML = 'Hi';
